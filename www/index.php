e9a20758e4b202309d63cd7ccd80c7b0cf3e0f01
angelos (Dr ) <amarkos@gmail.com>
2009-08-13 15:55:56 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/catkgui@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ www/index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1" >
+<title>BiplotGUI: Interactive Biplots in R</title>
+<link href="http://r-forge.r-project.org/themes/rforge/styles/estilo1.css" rel="stylesheet" type="text/css" />
+</head>
+<body bgcolor="#ffffff"><font size="1">
+<p><! --- R-Forge Logo --- >
+<table cellspacing="0" cellpadding="0" width="100%" border="0">
+  <tbody>
+  <tr>
+    <td><a href="http://r-forge.r-project.org"><img alt="R-Forge Logo" 
+      src="http://r-forge.r-project.org/themes/rforge//images/logo.png" 
+      border=0> </a></td></tr></tbody></table><!-- get project title  --><!-- own website starts here, the following may be changed as you like --></p>
+
+<h2>The caTkGUI package homepage </h2>
+<p><font size="2">The caTkGUI package for R is a Tcl/Tk GUI for all functions in the ca package.</font> </p>
+<p>
+<table cellspacing="0" cellpadding="0" width="750" align="left" border="0">
+  <tbody>
+  <tr>
+    <td>
+      <h4>Correspondence Analysis</h4>
+      <p><font size="2"></font> </p>
+
+      <hr>
+
+      <h4>Features</h4>
+      <ul>
+        <li>
+        <div><font size="2">A&nbsp;graphical user interface (GUI), making the 
+        package easy to use. </font></div>
+          <li>
+        <div><font size="2">The package 
+        is free and open-source.</font> </div></li></ul>
+      <h4>Requirements 
+      </h4>
+      <ul>
+        <li>
+      <div><font size="2">R needs to be installed. <a 
+        href="http://cran.r-project.org/bin/windows/base/">R 2.8.1</a> can 
+        &nbsp;<font size="2">be downloaded from <a 
+        href="http://cran.r-project.org">CRAN</a>    
+        . </font></font> 
+      </div>
+
+        <li>
+      <div><font size="2">At present, caTkGUI runs only in Windows.</font> 
+      </div></li></ul>
+      <h4>Installation 
+      </h4>
+      <ul>
+        <li>
+      <div><font size="2">To install the caTkGUI package 
+        and all its dependencies from within R, the following command can be 
+        entered at the&nbsp;prompt of the R console: <font 
+        face=Courier>install.packages("caTkGUI")</font><font 
+        face=Arial>.</font></font> 
+      </div>
+
+        <li>
+      <div><font size="2">The package dependencies (<a 
+        href="http://cran.r-project.org/web/packages/colorspace/index.html">ca</a>          
+          ,<span lang="EN">, <a 
+        href="http://cran.r-project.org/web/packages/rgl/index.html">rgl</a>, 
+        tcltk, <a 
+        href="http://cran.r-project.org/web/packages/tcltk2/index.html">tcltk2</a>, 
+        <a 
+        href="http://cran.r-project.org/web/packages/tkrplot/index.html">tkrplot</a></span>) 
+        also need to be installed.</font> 
+      </div></li></ul>
+
+      <h4><a href="BiplotGUIManual.pdf">Manual</a> </h4>
+      <h4><a href="Screenshots/index.html">Screenshots</a> </h4>
+      <h4><a href="Examples/index.html">Examples</a> </h4>
+      <hr>
+       <font size="2">
+      <p>
+
+      <p></p></font>
+      <p><font size="2">Click </font><a 
+      href="http://r-forge.r-project.org/projects/catkgui/"><font 
+      size=2>here</font></a><font size="2"> for help, to report bugs, or to 
+      download the developement version of the package.</font></p>
+      <p>
+      <hr>
+
+      <p></p>
+
+      <h4>Author </h4>
+
+      <p><font size="2">Angelos Markos</font> </p>
+      <h4>References </h4>
+  
+      <h4>License </h4>
+      <p><font size="2">Permission is granted to copy, distributed and/or modify 
+      the code of the caTkGUI package under the terms of the </font><a 
+      href="http://www.r-project.org/Licenses/GPL-3"><font size="2">GNU Public 
+      License, Version 3 </font></a><font size="2">or any later version published 
+      by the </font><a href="http://www.fsf.org/"><font size="2">Free Software 
+      Foundation </font></a><font size="2">.</font> </p>
+      <h4><font size="1">Last updated:&nbsp;13 August 2009</font> 
+  </h4></td></tr></tbody></table></p>
+<p>&nbsp;</p>
+
+<p>&nbsp;</p></font>
+</body>
+</html>
